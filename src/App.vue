e7e644a252b5cdb0a33c0acae0acd998ba63e308
bradocchs <bradspencer@covenantchristian.org>
2020-03-25 15:50:57 -0400

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,49 @@
 <template>
   <div id="app">
     <h1>Announcement Manager</h1>
+    <Announcement v-for="item in announcements" :key="item.id" :item="item" :user="user"/>
   </div>
 </template>
 
 <script>
+import Announcement from '@/components/Announcement'
+
 export default {
   name: 'App',
   components: {
+    Announcement
+  },
+  data: () => {
+    return {
+      announcements: [
+        { id: 1, content: 'This is a test.', days: 3, user: 'brad' },
+        { id: 2, content: 'This is also a test.', days: 1, user: 'bob' }
+      ],
+      user: 'brad'
+    }
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Arvo|Oswald&display=swap');
+
+body {
+  background: #b5c2ab;
+  margin: 0;
+  padding: 0;
+}
+
+h1 {
+  font-family: Oswald, sans-serif;
+  text-transform: uppercase;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arvo, serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Announcement.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="item">
+    <div><span v-if="owner" class="minus" @click="minus">-</span><span class="days">{{item.days}} {{days}}</span><span v-if="owner" class="plus" @click="plus">+</span></div>
+    <div class="content" :contenteditable="owner">{{item.content}}</div>
+    <div><span v-if="owner" class="remove" @click="remove">X</span><span class="user">{{item.user}}</span></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      days() {
+        return (this.item.days === 1) ? 'day' : 'days'
+      },
+      owner() {
+        return this.item.user === this.user
+      }
+    },
+    methods: {
+      minus() {
+        if (this.item.days > 1) this.item.days--
+      },
+      plus() {
+        if (this.item.days < 7) this.item.days++
+      },
+      remove() {
+        console.log('remove item', this.item.id)
+      }
+    },
+    props: {
+      item: {
+        type: Object,
+        required: true
+      },
+      user: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.content {
+  font-size: 1.3em;
+  outline: 0;
+  padding: 1vw;
+}
+.content:focus {
+  background: #fefdf9;
+  border-radius: 1vw;
+}
+.days {
+  display: inline-block;
+  margin: 1vw;
+  white-space: nowrap;
+  user-select: none;
+}
+.item {
+  background: #eeceab;
+  border: 1px solid #888888;
+  border-radius: 1vw;
+  display: flex;
+  justify-content: space-between;
+  margin: 4vw;
+  padding: 2vw;
+}
+.minus, .plus {
+  background: #b1d6e1;
+  border: 1px solid #888888;
+  border-radius: 10px;
+  display: inline-block;
+  font-weight: bold;
+  padding: 5px;
+  user-select: none;
+  width: 26px;
+}
+.remove {
+  background: #c7a3a1;
+  border: 1px solid #888888;
+  border-radius: 10px;
+  display: inline-block;
+  font-weight: bold;
+  padding: 5px;
+  user-select: none;
+  width: 26px;
+}
+.minus:hover, .plus:hover, .remove:hover {
+  cursor: pointer;
+}
+.user {
+  display: block;
+}
+</style>
